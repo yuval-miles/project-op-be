5d4aad5b6f4c01aad373cc0dbc102fe746fea869
yuval miles <yuvalmiles@gmail.com>
2022-09-06 14:38:21 +0300

    Init database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
-
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 @Module({
-  imports: [],
+  imports: [PrismaModule, ConfigModule.forRoot({ isGlobal: true }), UserModule],
 })
 export class AppModule {}
