8311cbdf3575a0795354beaeb708abba9383c704
yuval miles <yuvalmiles@gmail.com>
2022-09-07 18:55:21 +0300

    Cookies +cors

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,12 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: 'http://127.0.0.1:5173',
+      credentials: true,
+    },
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: false })); //input validator
   app.use(cookieParser());
   await app.listen(3000);
