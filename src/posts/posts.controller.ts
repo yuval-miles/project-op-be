d7f4af8f8cf89125614be5c2305f6013e5d50c93
Gregory <gregoryKot@gmail.com>
2022-09-11 12:46:50 +0300

    guard

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -14,24 +14,22 @@ import { Response } from 'express';
 import { PostsService } from './posts.service';
 import { JwtAuthGuard } from 'src/auth/guard/jwt.guard';
 import { FilterDto } from './dto/posts-filter.dto';
-
+@UseGuards(JwtAuthGuard)
 @Controller('posts')
+@UseGuards(JwtAuthGuard)
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
-  @UseGuards(JwtAuthGuard)
   @Post('createPost')
   createPost(@Body() dto: PostDto, @Res() res: Response) {
     return this.postsService.createPost(dto, res);
   }
 
-  // @UseGuards(JwtAuthGuard)
   @Delete(':id')
   deletePost(@Param() params: { id: string }, @Res() res: Response) {
     return this.postsService.deletePost(params.id, res);
   }
 
-  // @UseGuards(JwtAuthGuard)
   @Get()
   getPosts(@Res() res: Response, @Query() filterDto: FilterDto) {
     return this.postsService.getAllPosts(filterDto, res);
